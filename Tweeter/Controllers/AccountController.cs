f520f2567e24b013a2638589e4a15228bea8dc21
zztop <zaryab.ahmad@gmail.com>
2012-05-06 23:29:40 -0400

    Moved all the files to git drectory

---
+++ Tweeter/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Twitterizer;
+
+namespace Tweeter.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult LogOn()
+        {
+            string callbackUrl = string.Concat(
+                Request.Url.GetLeftPart(UriPartial.Authority),
+                VirtualPathUtility.ToAbsolute(this.Url.Action("CallBack", "Account")));
+            var token = OAuthUtility.GetRequestToken(
+                 ConfigurationManager.AppSettings["Twitterizer.ConsumerKey"],
+                 ConfigurationManager.AppSettings["Twitterizer.ConsumerSecret"],
+                 callbackUrl).Token;
+
+            return Redirect(OAuthUtility.BuildAuthorizationUri(token, true).ToString());
+        }
+        //
+        // GET: /Account/Callback
+
+        public ActionResult CallBack(string oauth_token, string oauth_verifier)
+        {
+            var tokens = OAuthUtility.GetAccessToken(
+                ConfigurationManager.AppSettings["Twitterizer.ConsumerKey"],
+                ConfigurationManager.AppSettings["Twitterizer.ConsumerSecret"],
+                oauth_token,
+                oauth_verifier);
+            Session["UserToken"] = tokens;
+            
+            FormsAuthentication.SetAuthCookie(tokens.ScreenName, false);
+            return RedirectToRoute(new {controller = "UserInfo", action = "Index", id = tokens.ScreenName});
+        }
+    }
+}
---
+++ Tweeter/Controllers/UserInfoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Twitterizer;
+
+namespace Tweeter.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        private OAuthTokenResponse _token;
+        //
+        // GET: UserAccount/Index
+
+        public ActionResult Index(string id)
+        {
+            ViewBag.UserId = id;
+
+            if (User != null && User.Identity.IsAuthenticated)
+            {
+                _token = Session["UserToken"] as OAuthTokenResponse;
+            }
+
+            return View();
+        }
+
+    }
+}
