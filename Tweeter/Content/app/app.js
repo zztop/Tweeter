bc6f9ac22ae78da9cf2c01dc560be9d9babedffd
zztop <zaryab.ahmad@gmail.com>
2012-06-25 00:50:02 -0400

    Restructured everything

---
+++ Tweeter/Content/app/app.js
@@ -0,0 +1,31 @@
+﻿define([
+// Libraries.
+  "jquery",
+  "underscore",
+  "backbone",
+  "modenizr",
+  "json",
+
+// Plugins.
+  "libs/bootstrap/js/bootstrap"
+],
+function ($, _, Backbone) {
+
+    // Patch collection fetching to emit a `fetch` event.
+    Backbone.Collection.prototype.fetch = function () {
+        var fetch = Backbone.Collection.prototype.fetch;
+
+        return function () {
+            this.trigger("fetch");
+
+            return fetch.apply(this, arguments);
+        };
+    } ();
+
+    // Provide a global location to place configuration settings and module
+    // creation.
+    var app = {
+        // The root path to run the application through.
+        root: "/"
+    };
+});
---
+++ Tweeter/Content/app/config.js
@@ -0,0 +1,30 @@
+﻿require.config({
+
+    // Initialize the application with the main application file.
+    deps: ["main"],
+
+    paths: {
+        // JavaScript folders.
+        libs: "libs",
+        app: "app",
+        helper:"libs/helper",
+        // Libraries.
+        jquery: "libs/jquery/jquery",
+        underscore: "libs/underscore/underscore",
+        backbone: "libs/backbone",
+        modenizr:"helper/modenizr",
+        json:"helper/json2"
+    },
+
+    shim: {
+        // Backbone library depends on lodash and jQuery.
+        backbone: {
+            deps: ["underscore", "jquery"],
+            exports: "Backbone"
+        },
+
+        // Twitter Bootstrap depends on jQuery.
+        "libs/bootstrap/js/bootstrap": ["jquery"]
+    }
+
+});
